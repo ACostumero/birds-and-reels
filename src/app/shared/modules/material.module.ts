69d8ff8df776579efeaa86f6825a94a3f7aa4d57
ACostumero <aitorcostu@gmail.com>
2023-02-18 19:22:55 +0100

    feat: Add svg icons and create icon module for manage them

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
+import { IconModule } from "@app-shared/modules/icon.module";
 
 
 const modules = [
   CommonModule,
+  IconModule,
   MatSlideToggleModule,
   MatToolbarModule,
   MatIconModule,
@@ -22,6 +24,7 @@ const modules = [
   declarations: [],
   imports: [modules],
   exports: [modules],
+  providers: [MatIconRegistry]
 })
 export class MaterialModule {
 }
--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from "@app-shared/components/card/card.component";
+import { MaterialModule } from "@app-shared/modules/material.module";
+import { HttpClientModule } from "@angular/common/http";
 
-
+const components = [
+  CardComponent
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [components],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    HttpClientModule
+  ],
+  exports: [
+    components,
+    MaterialModule
   ]
 })
 export class SharedModule { }
