8ac6918c361c9b07fd61d9368c5a05d5002e0972
ACostumero <aitorcostu@gmail.com>
2023-03-27 21:07:50 +0200

    feat: Create entry component

--- src/app/features/entry/container/entry.component.ts
+++ src/app/features/entry/container/entry.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {EntryFacade} from "@app-features/entry/facades/entry.facade";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {filter, map, Subject, takeUntil, tap} from "rxjs";
+import {TEntryId} from "@app-core/types/entry.type";
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.scss']
 })
-export class EntryComponent {
+export class EntryComponent implements OnInit, OnDestroy {
 
-  constructor() {
-    console.log('entry')
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _entryFacade: EntryFacade) {}
+
+  private _destroySubscriptions$: Subject<void> = new Subject<void>();
+
+  public entry$ = this._entryFacade.entry$;
+
+  ngOnInit() {
+    this._activatedRoute.paramMap.pipe(
+      takeUntil(this._destroySubscriptions$),
+      tap(() => this._entryFacade.setEntryId(null)),
+      map((params: ParamMap) => params.get('id')),
+      filter(Boolean),
+      tap((entryId: TEntryId) => {
+        this._entryFacade.setEntryId(entryId)
+      })
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this._destroySubscriptions$.next();
   }
 
 }
